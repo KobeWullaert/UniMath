7953382825118f48961e198afbc042f0d6e34ff0
kfwullaert <kwullaer@hotmail.com>
2025-08-23 15:10:17 +0200

    Prelim in groupoid vs dagger

--- UniMath/CategoryTheory/DaggerCategories/Examples/Groupoids.v
+++ UniMath/CategoryTheory/DaggerCategories/Examples/Groupoids.v
@@ -1,5 +1,16 @@
-(* Any groupoid becomes a †-category by defining f^† := f^-1
-Furthermore, a groupoid is dagger univalent if and only if it is univalent.
+(*
+        Groupoids As Dagger Categories
+
+        Any groupoid becomes a †-category by defining f^† := f^-1.
+        Furthermore, a groupoid is dagger univalent if and only if it is univalent.
+        Also, every functor between groupoids is dagger-preserving
+
+        Contents.
+        1. Groupoids are dagger categories
+        2. Univalence for groupoidal dagger categories
+        3. Functors between groupoids are dagger-preserving
+        4. The core of a dagger category is a dagger category
+        5. Weak equivalences between dagger categories are dagger weak equivalences
 *)
 
 Require Import UniMath.Foundations.All.
@@ -9,16 +20,20 @@ Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.Core.Functors.
 Require Import UniMath.CategoryTheory.Core.NaturalTransformations.
 Require Import UniMath.CategoryTheory.Core.Isos.
+Require Import UniMath.CategoryTheory.WeakEquivalences.Core.
 Require Import UniMath.CategoryTheory.Core.Univalence.
 
 Require Import UniMath.CategoryTheory.Groupoids.
 
 Require Import UniMath.CategoryTheory.DaggerCategories.Categories.
+Require Import UniMath.CategoryTheory.DaggerCategories.Functors.
 Require Import UniMath.CategoryTheory.DaggerCategories.Unitary.
 Require Import UniMath.CategoryTheory.DaggerCategories.Univalence.
+Require Import UniMath.CategoryTheory.DaggerCategories.Functors.WeakEquivalences.
 
 Local Open Scope cat.
 
+(** * 1. Groupoids Are Dagger Categories *)
 Section GroupoidsAsDaggers.
 
   Context (C : groupoid).
@@ -48,6 +63,7 @@ Section GroupoidsAsDaggers.
 
 End GroupoidsAsDaggers.
 
+(** * 2. Univalence For Groupoidal Dagger Categories *)
 Section UnivalenceOfGroupoids.
 
   Context (C : groupoid).
@@ -110,3 +126,203 @@ Section UnivalenceOfGroupoids.
   Qed.
 
 End UnivalenceOfGroupoids.
+
+(** * 3. Functors Are Dagger Preserving *)
+Section FunctorsBetweenGroupoidsAreDaggerPreserving.
+
+  Context {C D : groupoid} (F : functor C D).
+
+  Lemma functor_between_groupoids_is_dagger_functor
+    : is_dagger_functor (GRP_dagger C) (GRP_dagger D) F.
+  Proof.
+    intros a b f.
+    cbn.
+    refine (functor_on_inv_from_z_iso F (f ,, groupoid_is_pregroupoid C a b f) @ _).
+    apply maponpaths.
+    use subtypePath.
+    { intro ; apply isaprop_is_z_isomorphism. }
+    apply idpath.
+  Qed.
+
+End FunctorsBetweenGroupoidsAreDaggerPreserving.
+
+(** * 4. Core Of A Dagger Category *)
+Section CoreOfDaggerCategory.
+
+  Context {C : category} (d : dagger C).
+
+  Definition core_of_dagger_cat_data : precategory_data.
+  Proof.
+    use make_precategory_data.
+    - use tpair.
+      + exact (ob C).
+      + exact (unitary d).
+    - apply unitary_id.
+    - intros a b c.
+      apply unitary_comp.
+  Defined.
+
+  Definition core_of_dagger_cat_ax : is_precategory core_of_dagger_cat_data.
+  Proof.
+    repeat split ; intro ; intros
+    ; apply unitary_eq
+    ; apply (pr21 C).
+  Qed.
+
+  Definition core_of_dagger_precat : precategory.
+  Proof.
+    use make_precategory.
+    - exact core_of_dagger_cat_data.
+    - exact core_of_dagger_cat_ax.
+  Defined.
+
+  Definition core_of_dagger_cat : category.
+  Proof.
+    use make_category.
+    - exact core_of_dagger_precat.
+    - intro ; intro ; apply isaset_unitary.
+  Defined.
+
+  Definition dagger_on_core_of_dagger_cat
+    : dagger core_of_dagger_cat.
+  Proof.
+    use tpair.
+    - intro ; intro ; apply unitary_inv.
+    - abstract (repeat split ; intro ; intros
+      ; use unitary_eq ; apply (pr2 d)).
+  Defined.
+
+  Definition core_dagger_cat_is_groupoid : groupoid.
+  Proof.
+    exists core_of_dagger_cat.
+    intros a b f.
+    set (i := make_z_iso _ _ (pr2 f)).
+    use tpair.
+    - exact (unitary_inv f).
+    - split ; use unitary_eq.
+      + exact (pr122 i).
+      + exact (pr222 i).
+  Defined.
+
+  Lemma dagger_on_groupoid_core_is_groupoid_dagger
+    : GRP_dagger core_dagger_cat_is_groupoid = dagger_on_core_of_dagger_cat.
+  Proof.
+    use subtypePath.
+    { intro ; apply isaprop_dagger_laws. }
+    do 3 (apply funextsec ; intro) ; apply idpath.
+  Qed.
+
+  Definition groupoid_inclusion_is_dagger_functor
+    : dagger_functor dagger_on_core_of_dagger_cat d.
+  Proof.
+    use make_dagger_functor.
+    - use make_functor.
+      + use make_functor_data.
+        * exact (λ x, x).
+        * exact (λ _ _ f, pr1 f).
+      + abstract (split ; intro ; intros ; apply idpath).
+    - split.
+  Defined.
+
+  Lemma unitary_in_dagger_core (c c' : C)
+    : unitary dagger_on_core_of_dagger_cat c c' ≃ unitary d c c'.
+  Proof.
+    use weq_iso.
+    - exact pr1.
+    - intro u.
+      exists u.
+      split ; apply unitary_eq ; apply (pr2 u).
+    - intro ; apply unitary_eq, idpath.
+    - intro ; apply unitary_eq, idpath.
+  Defined.
+
+  Lemma idtouiso_in_dagger_code (c c' : C)
+    : idtodaggeriso d c c'
+      = funcomp (idtodaggeriso dagger_on_core_of_dagger_cat c c') (unitary_in_dagger_core c c').
+  Proof.
+    apply funextsec ; intro p.
+    induction p.
+    apply idpath.
+  Qed.
+
+  Lemma univalence_of_dagger_core
+    : is_univalent_dagger d ≃ is_univalent_dagger dagger_on_core_of_dagger_cat.
+  Proof.
+    use weqonsecfibers.
+    intro c.
+    use weqonsecfibers.
+    intro c'.
+    refine (_ ∘ weq_transportf isweq (idtouiso_in_dagger_code c c'))%weq.
+    use weqimplimpl ; try (apply isapropisweq).
+    - intro p.
+      use (twooutof3a _ _ p).
+      apply unitary_in_dagger_core.
+    - intro p.
+      use (twooutof3c _ _ p).
+      apply unitary_in_dagger_core.
+  Qed.
+
+End CoreOfDaggerCategory.
+
+(** * 5. Weak Equivalences *)
+Section WeakEquivalences.
+
+  Lemma weak_equiv_preserves_groupoidalness
+    {C D : category} (F : weak_equiv C D)
+    : is_pregroupoid C → is_pregroupoid D.
+  Proof.
+    intro Cg.
+    intros d d' f.
+    use (factor_through_squash _ _ (eso_from_weak_equiv _ (pr2 F) d)).
+    { apply isaprop_is_z_isomorphism. }
+    intros [c id].
+    use (factor_through_squash _ _ (eso_from_weak_equiv _ (pr2 F) d')).
+    { apply isaprop_is_z_isomorphism. }
+    intros [c' id'].
+
+    assert (pf : f = inv_from_z_iso id
+                       · #(pr1 F) (fully_faithful_inv_hom (pr22 F) _ _ (id · f · inv_from_z_iso id'))
+                       · id'
+           ).
+    {
+      rewrite functor_on_fully_faithful_inv_hom.
+      rewrite ! assoc.
+      rewrite z_iso_after_z_iso_inv, id_left.
+      now rewrite assoc', z_iso_after_z_iso_inv, id_right.
+    }
+    induction (! pf).
+    repeat (use is_z_isomorphism_comp).
+    - apply z_iso_inv.
+    - apply functor_on_is_z_isomorphism.
+      apply Cg.
+    - apply id'.
+  Qed.
+
+  Lemma eso_between_groupoids_is_dagger_eso
+    {C D : groupoid} {F : functor C D} (F_eso : essentially_surjective F)
+    : is_unitarily_eso (functor_between_groupoids_is_dagger_functor F).
+  Proof.
+    intro d.
+    use (factor_through_squash _ _ (F_eso d)).
+    { apply isapropishinh. }
+    intros [c i].
+    apply hinhpr.
+    exists c.
+    apply univalence_to_dagger_univalence.
+    exact i.
+  Qed.
+
+  Lemma dagger_weak_equivalences_between_groupoids
+    {C D : groupoid} (F : functor C D)
+    : is_weak_equiv F ≃ is_weak_dagger_equiv (functor_between_groupoids_is_dagger_functor F).
+  Proof.
+    use weqdirprodf.
+    2: { apply idweq. }
+    use weqimplimpl.
+    - apply eso_between_groupoids_is_dagger_eso.
+    - apply is_unitarily_eso_is_eso.
+    - apply isaprop_essentially_surjective.
+    - apply isaprop_is_unitarily_eso.
+  Qed.
+
+End WeakEquivalences.
